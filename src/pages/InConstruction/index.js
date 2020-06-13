27a50149310654bda7930f6d0ca6f8faf413674e
alfa9317 <josealfredotg@gmail.com>
2020-06-12 20:08:21 -0500

    initial push and setup

---
+++ src/pages/InConstruction/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {makeStyles} from '@material-ui/styles';
+import {Animated} from "react-animated-css";
+import constLogo from '../../assets/images/constLogo.png'
+
+const useStyles = makeStyles(theme => ({
+    main: {
+       textAlign: 'center',
+       backgroundColor: '#141414',
+       color: 'white',
+       fontFamily: 'Mukta',
+       fontSize: '1.5vw',
+       [theme.breakpoints.down('sm')]: {
+        fontSize: '3vw'
+        //Page setup
+    }
+    }
+}))
+
+export default function InConstruction(props){
+
+        const classes = useStyles();
+        return (
+            
+            <div className={classes.main}>
+                 {/* <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={1000} isVisible={true}>
+                    <div>
+                        <h2 className='homePageTitle' style={{fontSize:'3vw', fontFamily: 'Mukta'}}>¡Bienvenidos!</h2>
+                        <br/>
+                    </div>
+                </Animated> */}
+                
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.6} animationInDuration={3000} isVisible={true}>
+                    <img alt="profilePic" src={constLogo} style={{width: '50vw',height: 'auto'}}/>
+                </Animated>
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={3.6} animationInDuration={4000} isVisible={true}>
+                    <p>El sitio está temporalmente en construcción, disculpe las molestias</p>
+                </Animated>
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={3.7} animationInDuration={4000} isVisible={true}>
+                    <p style={{margin:'0px 15vw 0px 15vw'}}>Teléfono: (352) 125 0143; Correo electrónico: denstela.contacto@gmail.com</p>
+                </Animated>
+             
+                <div className='homePageTitle' style={{fontSize:'100px'}}>...</div>
+            </div>
+            
+          );
+  }
