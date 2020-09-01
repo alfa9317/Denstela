e43a776e565005f38be85b012213d913bc8308ef
alfa9317 <josealfredotg@gmail.com>
2020-09-01 16:17:13 -0500

    Header basic implementation and first approach

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css'
 import theme from './components/ui/Theme';
+import Header from './components/ui/Header';
 import InConstruction from './pages/InConstruction';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <Header/>
         <Switch>
           <Route exact path='/' component={InConstruction} />
         </Switch>
