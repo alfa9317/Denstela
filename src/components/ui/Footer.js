b0089f9418a8fdcc7475505bbdf5801fa38b3d1e
alfa9317 <josealfredotg@gmail.com>
2020-09-17 18:13:11 -0500

    First approach to the grid structure inside the footer

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 import footerAdornment from '../../assets/images/constLogo2.png'
 
@@ -9,7 +10,7 @@ const useStyles = makeStyles(theme => ({
         width: "100%",
         zIndex: 1302,
         position: "relative",
-        // height: "5.5em",
+        height: "10rem",
         // [theme.breakpoints.down('md')]:{
         //     height: "17em"
         // },
@@ -26,6 +27,15 @@ const useStyles = makeStyles(theme => ({
         [theme.breakpoints.down('xs')]:{
             width: "14em"
         }
+    },
+    linksContainer: {
+
+    },
+    link: {
+        color: 'white',
+        fontFamily: "'Montserrat', sans-serif",
+        fontSize: "0.75rem",
+        fontWeight: "bold"
     }
 }))
 
@@ -33,6 +43,61 @@ export default function Footer(){
     const classes = useStyles();
 
     return  <footer className={classes.footer}>
-                <img alt="logo" src={footerAdornment} className={classes.adornment}/> 
+                <Grid container spacing={40}>
+                     <Grid item xs={12} sm={4}> 
+                        <img alt="logo" src={footerAdornment} className={classes.adornment}/>
+                     </Grid>
+                     <Grid item xs={12} sm={4}>
+                         <Grid container justify="center" className={classes.linksContainer} spacing={3}>
+                             <Grid item>
+                                 <Grid container direction="column" spacing={1}>
+                                    <Grid item className={classes.link}>
+                                        Inicio
+                                    </Grid>
+                                 </Grid>
+                             </Grid>
+                             <Grid item>
+                                 <Grid container direction="column" spacing={1}>
+                                    <Grid item className={classes.link}>
+                                        Servicios
+                                    </Grid>
+                                    <Grid item className={classes.link}>
+                                        Servicio 1
+                                    </Grid>
+                                    <Grid item className={classes.link}>
+                                        Servicio 2
+                                    </Grid>
+                                    <Grid item className={classes.link}>
+                                        Servicio 3
+                                    </Grid>
+                                 </Grid>
+                             </Grid>
+                             <Grid item>
+                                 <Grid container direction="column" spacing={1}>
+                                    <Grid item className={classes.link}>
+                                        Sobre nosotros
+                                    </Grid>
+                                 </Grid>
+                             </Grid>
+                             <Grid item>
+                                 <Grid container direction="column" spacing={1}>
+                                    <Grid item className={classes.link}>
+                                        Casos de éxito
+                                    </Grid>
+                                 </Grid>
+                             </Grid>
+                             <Grid item>
+                                 <Grid container direction="column" spacing={1}>
+                                    <Grid item className={classes.link}>
+                                        Contáctanos
+                                    </Grid>
+                                 </Grid>
+                             </Grid>
+                         </Grid>
+                     </Grid>
+                     <Grid item justify="center" xs={12} sm={4}>
+                         Botones
+                     </Grid>
+                </Grid> 
             </footer>
 }
